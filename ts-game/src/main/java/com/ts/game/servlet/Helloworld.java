14bbec718618fdcf96e72d0ce0b37a16b3152799
ekkapong.b <ekkapong.bo@gmail.com>
2017-12-15 21:33:27 +0700

    bugfixed response json.

--- ts-game/src/main/java/com/ts/game/servlet/Helloworld.java
+++ ts-game/src/main/java/com/ts/game/servlet/Helloworld.java
@@ -1,7 +1,6 @@
 package com.ts.game.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.ts.game.model.ExampleModel;
 
 /**
  * Servlet implementation class Helloworld
@@ -31,24 +31,41 @@ public class Helloworld extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	Gson gson = new Gson();
-	String objectToReturn = "{ key1: 'value1', key2: 'value2' }";
+	try {
+	    Gson gson = new Gson();
+	    ExampleModel exampleModel = new ExampleModel();
 
-	response.setContentType("application/json;charset=UTF-8");
-	PrintWriter out = response.getWriter();
-	// Assuming your json object is **jsonObject**, perform the following, it will return your json object  
-	out.print(objectToReturn);
-	out.flush();
-//	try (PrintWriter out = response.getWriter()) {
-//	    ExampleModel exampleModel = new ExampleModel();
-//
-//	    exampleModel.setName("Test");
-//	    exampleModel.setPassword("Password");
-//
-//	    out.print(gson.toJson(exampleModel));
-//	} catch (Exception e) {
-//	    e.printStackTrace();
-//	}
+	    exampleModel.setName("Test");
+	    exampleModel.setPassword("Password");
+
+	    response.setContentType("application/json");
+
+	    response.getOutputStream().print(gson.toJson(exampleModel));
+	    response.getOutputStream().flush();
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	    // status.setSuccess(false);
+	    // status.setDescription(ex.getMessage());
+	    // response.getOutputStream().print(gson.toJson(status));
+	    // response.getOutputStream().flush();
+	}
+
+	// response.setContentType("application/json;charset=UTF-8");
+	// PrintWriter out = response.getWriter();
+	// // Assuming your json object is **jsonObject**, perform the following, it
+	// will return your json object
+	// out.print(objectToReturn);
+	// out.flush();
+	// try (PrintWriter out = response.getWriter()) {
+	// ExampleModel exampleModel = new ExampleModel();
+	//
+	// exampleModel.setName("Test");
+	// exampleModel.setPassword("Password");
+	//
+	// out.print(gson.toJson(exampleModel));
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
 
     }
 
